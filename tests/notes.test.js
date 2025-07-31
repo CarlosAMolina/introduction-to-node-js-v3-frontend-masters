ce3a5d59ea9ba25e557b4f1813883b15dc09e453
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-07-31 17:55:30 +0200

    test(notes): add test

---
+++ tests/notes.test.js
@@ -0,0 +1,33 @@
+import { jest } from '@jest/globals';
+
+jest.unstable_mockModule('../src/db.js', () => ({
+  insertDB: jest.fn(),
+  getDB: jest.fn(),
+  saveDB: jest.fn(),
+}));
+
+// To mock the db functions. First we mock and then import them dynamically.
+const { insertDB, getDB, saveDB } = await import('../src/db.js');
+const { newNote, getAllNotes, removeNote } = await import('../src/notes.js');
+
+beforeEach(() => {
+  insertDB.mockClear();
+  getDB.mockClear();
+  saveDB.mockClear();
+})
+
+
+test('newNote inserts data and returns it', async () => {
+  const note = {
+    content: 'this is my note',
+    id: 1,
+    tags: ['hello']
+  }
+  // We resolve the promise.
+  insertDB.mockResolvedValue(note);
+
+  const result = await newNote(note.content, note.tags);
+  expect(result.content).toEqual(note.content);
+  expect(result.tags).toEqual(note.tags);
+});
+
