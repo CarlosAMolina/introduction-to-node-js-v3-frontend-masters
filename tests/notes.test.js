9808d7fbe2bbbbbc8cf53773e0ee13b34ef4f8f7
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-07-31 17:59:57 +0200

    test(notes): add group in describe

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -16,39 +16,41 @@ beforeEach(() => {
   saveDB.mockClear();
 })
 
+describe('cli app', () => {
 
-test('newNote inserts data and returns it', async () => {
-  const note = {
-    content: 'this is my note',
-    id: 1,
-    tags: ['hello']
-  }
-  insertDB.mockResolvedValue(note);
-
-  const result = await newNote(note.content, note.tags);
-  expect(result.content).toEqual(note.content);
-  expect(result.tags).toEqual(note.tags);
-});
-
-test('getAllNotes returns all notes', async () => {
-  const db = {
-    notes: ['note1', 'note2', 'note3']
-  };
-  getDB.mockResolvedValue(db);
-
-  const result = await getAllNotes();
-  expect(result).toEqual(db.notes);
-});
-
-test('removeNote does nothing if id is not found', async () => {
-  const notes = [
-    { id: 1, content: 'note 1' },
-    { id: 2, content: 'note 2' },
-    { id: 3, content: 'note 3' },
-  ];
-  saveDB.mockResolvedValue(notes);
-
-  const idToRemove = 4;
-  const result = await removeNote(idToRemove);
-  expect(result).toBeUndefined();
-});
+  test('newNote inserts data and returns it', async () => {
+    const note = {
+      content: 'this is my note',
+      id: 1,
+      tags: ['hello']
+    }
+    insertDB.mockResolvedValue(note);
+  
+    const result = await newNote(note.content, note.tags);
+    expect(result.content).toEqual(note.content);
+    expect(result.tags).toEqual(note.tags);
+  });
+  
+  test('getAllNotes returns all notes', async () => {
+    const db = {
+      notes: ['note1', 'note2', 'note3']
+    };
+    getDB.mockResolvedValue(db);
+  
+    const result = await getAllNotes();
+    expect(result).toEqual(db.notes);
+  });
+  
+  test('removeNote does nothing if id is not found', async () => {
+    const notes = [
+      { id: 1, content: 'note 1' },
+      { id: 2, content: 'note 2' },
+      { id: 3, content: 'note 3' },
+    ];
+    saveDB.mockResolvedValue(notes);
+  
+    const idToRemove = 4;
+    const result = await removeNote(idToRemove);
+    expect(result).toBeUndefined();
+  });
+})
